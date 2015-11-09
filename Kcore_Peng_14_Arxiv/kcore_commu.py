58e315ab0611d62e2c6db61b20a4e6bddd976894
Lovingmage <skiphoopwow@gmail.com>
2015-11-09 12:03:24 -0500

    New version recover based on optimize modularity
    New version recover based on optimize modularity

--- Kcore_Peng_14_Arxiv/kcore_commu.py
+++ Kcore_Peng_14_Arxiv/kcore_commu.py
@@ -20,20 +20,42 @@
 
 import community
 import networkx as nx
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+#import matplotlib
+#matplotlib.use('Agg')
+#import matplotlib.pyplot as plt
+
 
 #Read Network files as gml file type, create a networkx graph and use the eisted graph file
 #Random graph may have poor performance, erdos renyi graph doesn't have true community structure
-H = nx.read_gml('netscience.gml')
-H = nx.k_core (H,2)
+G = nx.read_gml('dolphins.gml')
+H = nx.k_core (G,3)
 #Compute the best partition by using louvain method
+print G.nodes()
+print H.nodes()
 partition = community.best_partition(H)
 
+#print partition.values()
+#print partition[34]
 #Plot the result
-values = [partition.get(node) for node in H.nodes()]
-print H.number_of_nodes()
+#values = [partition.get(node) for node in H.nodes()]
+#print H.number_of_nodes()
 
-nx.draw_spring(H, cmap = plt.get_cmap('jet'), node_color = values, node_size=30, with_labels=False)
-plt.savefig('netscienceKcore2')
+#nx.draw_spring(H, cmap = plt.get_cmap('jet'), node_color = values, node_size=30, with_labels=False)
+#plt.savefig('dolphinKcore3')
+
+#print partition.values()
+communities = list(set(partition.values()))
+print communities
+
+for nodes in G.nodes():
+	if nodes not in H.nodes():
+		best_mod = 0
+		for coms in communities:
+			partition[nodes] = coms
+			if community.modularity(partition, H) < best_mod:
+				del partition[nodes]
+
+print partition
+				
+
+
