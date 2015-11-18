b29969aa59ed35d07904915f96b2e5feca26908b
Lovingmage <skiphoopwow@gmail.com>
2015-11-17 23:02:03 -0500

    Add some comments, revised some bugs
    New version of current recover algorithm

--- Kcore_Peng_14_Arxiv/kcore_commu.py
+++ Kcore_Peng_14_Arxiv/kcore_commu.py
@@ -26,11 +26,15 @@ from collections import Counter
 import sys
 
 	
+#############################################################################################
+#	Function : convert_partition_format( ORIGINAL_PARTITION ):			
+#											
+#											
+#	This function will convert Partition Format from the original format 		
+#	to the one which we will used in community similarity comparison process.	
+#############################################################################################
 	
 def convert_partition_format(original_partition):
-#Read Network files as gml file type, create a networkx graph and use the eisted graph file
-#Random graph may have poor performance, erdos renyi graph doesn't have true community structure
-	#partition = community.best_partition(H)
 	communities = list(set(partition.values()))
 	new_partition = {}
 	for community_part in communities:
@@ -40,8 +44,18 @@ def convert_partition_format(original_partition):
         	new_partition[partition[nodes]].append(nodes)
 	#print new_partition
 	return new_partition
-	
 
+
+
+##############################################################################################
+#       Function : sort_by_neighbor( Kcore Subgraph H, Oroginal Graph G ):		     
+#											     
+#											
+#       This function will sort the recover nodes based on the inside-kcore neighbors,  
+#       the function will receive two graph H and G, and then return the asscending     
+#	ordered nodes sequence which need to be recovered.			        
+##############################################################################################
+	
 def sort_by_neighbor(H, G):
 	neighbor_dict = {}
 	sorted_neighbor = []
@@ -63,12 +77,25 @@ def sort_by_neighbor(H, G):
 	return sorted_neighbor
 		
 
+
+##############################################################################################
+#       Function : vote_for_node( kcore partition kcore_partition, 
+#				  sort order recover nodes sorted_recover_nodes,
+#				  Original Graph G ):                   
+#                                                                                            
+#                                                                                       
+#       This function is the main function to recover the nodes into current partition,
+#	It will retuen a recovered partition dictionary, and if some nodes can not be 
+#	recovered, it will divide them into -1 community                              
+##############################################################################################
+
+
 def vote_for_node(kcore_partition, sorted_recover_nodes, G):
-	rec_partition = kcore_partition
+	#rec_partition = kcore_partition
 	for node in sorted_recover_nodes:
 		vote_list = []
 		for neighbor in G.neighbors(node):
-			if neighbor not in rec_partition.keys():
+			if neighbor not in kcore_partition.keys():
 				continue
 			else:
 				vote_list.append(kcore_partition[neighbor])
@@ -78,10 +105,10 @@ def vote_for_node(kcore_partition, sorted_recover_nodes, G):
 		vote_dic_sorted = sorted(vote_dic.iteritems(), key=lambda d:d[1], reverse = False )
 		#print vote_dic_sorted
 		if not vote_dic_sorted:
-			rec_partition = -1
+			kcore_partition[node] = -1
 		else:
-			rec_partition[node] = vote_dic_sorted[0][0]
-	return rec_partition	
+			kcore_partition[node] = vote_dic_sorted[0][0]
+	return kcore_partition	
 
 
 
@@ -89,14 +116,18 @@ if __name__ == '__main__':
 
 	command2 = sys.argv[1]
 	command3 = sys.argv[2]
+	#Read Network files as gml file type, create a networkx graph and use the eisted graph file
+	#Random graph may have poor performance, erdos renyi graph doesn't have true community structure
 	G = nx.read_gml(command2)
 	H = nx.k_core (G, int(command3))
+	print len(H.nodes())
 	#kcore_partition = kcore_partition(H)
 	partition = community.best_partition(H)
+	#print partition 
 	sorted_recover_nodes = sort_by_neighbor(H, G)
 	#print sorted_recover_nodes
 	vote_for_node(partition, sorted_recover_nodes, G)
 	new_partition = vote_for_node(partition, sorted_recover_nodes, G)
-
-	print new_partition
-		
+	print len(new_partition.keys())
+	convert = convert_partition_format(new_partition)
+	print len(convert[-1])	
