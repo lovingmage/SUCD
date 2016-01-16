533bbd10cb5d42345ae48b8941f8ac74c88f33d6
Lovingmage <skiphoopwow@gmail.com>
2016-01-16 00:35:51 -0500

    Comments Adds
    Comments Adds

--- Kcore_Peng_14_Arxiv/Kcore_Comparison_with_diff_k/SNAP/kcore_commu.py
+++ Kcore_Peng_14_Arxiv/Kcore_Comparison_with_diff_k/SNAP/kcore_commu.py
@@ -26,17 +26,63 @@ import time
 
 #This function is used to create the node vector called NodeV of a given graph
 def create_node_vector(G):
+	"""Create Node Vectors by extracting given graph G .
+    Parameters
+    ----------
+    G : snap graph
+    source : node, optional
+       
+    Returns
+    -------
+    NodeV: TIntV
+       A TIntV vector which store all node ids of the given graph.
+    Examples
+    --------
+    >>> NodeV_G = create_node_vector(G)
+    >>> print NodeV_G
+    <snap.TIntV; proxy of <Swig Object of type 'TVec< TInt > *' at 0x108fa8ea0> >
+    Notes
+    -----
+    by C. Wang, Jan 16.
+    This code is used for convenient computing, for instance, we may use the 
+    intersection function of Vector structure when we want to recover Kcore
+    partition into original partition.
+    """
 	NodeV = snap.TIntV()
 	for node in G.Nodes():
 		NodeV.Add(node.GetId())
 	#print NodeV[200]
 	return NodeV
 
+
 def get_diff_from_kcore(NodeV_G,NodeV_Kcore):
+	"""This function is used to calculate the nodes set which are not in Kcore.
+	   Which are the missing nodes when we apply kcore based community detection.
+
+    Parameters
+    ----------
+    NodeV_G : Node Vector of original graph G, type TIntV
+    NodeV_Kcore : Node Vector of Kcore subgraph Kcore, type TIntV
+       
+    Returns
+    -------
+    NodeV_Diff: TIntV
+       A TIntV vector which store all node ids which appears in G but not in Kcore.
+
+    Examples
+    --------
+    >>> NodeV_Diff = get_diff_from_kcore(NodeV_G, NodeV_Kcore)
+    >>> print NodeV_Diff
+    <snap.TIntV; proxy of <Swig Object of type 'TVec< TInt > *' at 0x108fa8ea0> >
+    Notes
+    -----
+    by C. Wang, Jan 16.
+    """
 	NodeV_Diff = snap.TIntV()
 	NodeV_G.Diff(NodeV_Kcore, NodeV_Diff)
 	#print NodeV_Diff[0]
 	return NodeV_Diff
+
 
 def sort_by_neighbor(NodeV_Diff, Kcore, G):
 	SortH = snap.TIntIntH()
@@ -51,6 +97,7 @@ def sort_by_neighbor(NodeV_Diff, Kcore, G):
 	#for key in temp_hash_table:
 	#	SortedV.Add(key)
 	return SortH
+
 
 def community_partition(G):
 	CommuV = snap.TCnComV()
@@ -77,6 +124,8 @@ def recover_partition_by_kcore(Partition_KcoreH, SortH, G):
 			Recover_PartitionH[node] = vote_for_node(node, Partition_KcoreH, G, Kcore)
 	return Recover_PartitionH
 
+
+
 def vote_for_node(node, PartitionH, G, Kcore):
 	VoteNodeH = snap.TIntIntH()
 	for neighbor in G.GetNI(node).GetOutEdges():
@@ -99,6 +148,7 @@ if __name__ == '__main__':
 	G = snap.LoadEdgeList(snap.PUNGraph, "facebook_combined.txt", 0, 1)
 	
 	NodeV_G = create_node_vector(G)
+	print NodeV_G
 	Kcore = snap.GetKCore(G, 5)
 	NodeV_Kcore = create_node_vector(Kcore)
 	NodeV_Diff = get_diff_from_kcore(NodeV_G, NodeV_Kcore)
