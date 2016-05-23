af602ed995e160602e506fd73a0a1e73ef96404f
Lovingmage <skiphoopwow@gmail.com>
2016-05-23 16:48:25 -0400

    new up
    new up

--- Anti Kcore/antikcore.py
+++ Anti Kcore/antikcore.py
@@ -1,11 +1,25 @@
 import networkx as nx
 
-def anit_kcore(G, k = None, core_number = None):
+def anti_kcore(G, k = None, core_number = None):
     if core_number is None:
         core_number = nx.core_number(G)
     if k is None:
         k = max(core_number.values())
     nodes = (n for n in core_number if core_number[n] >= k)
     anti_nodes = (n for n in core_number if core_number[n] < k)
-    return G.subgraph(anti_nodes).copy(), nodes
+    return (G.subgraph(anti_nodes).copy(), list(nodes))
     
+if __name__ == "__main__":
+    FILE_PATH = "./dolphin.txt"
+    G = nx.read_edgelist(FILE_PATH)
+    (H, kcore_list) = anti_kcore(G)
+    print H.nodes()
+    print kcore_list
+    
+    (M, kcore_list2) = anti_kcore(H)
+    print M.nodes()
+    print kcore_list2
+
+    
+    
+      
