95a61eca063fe269a6cfe54c033dd3979086a879
Lovingmage <skiphoopwow@gmail.com>
2016-05-25 11:21:02 -0400

    New Update for Anti Kcore
    Added the scale control function

--- Anti Kcore/antikcore.py
+++ Anti Kcore/antikcore.py
@@ -8,17 +8,30 @@ def anti_kcore(G, k = None, core_number = None):
     nodes = (n for n in core_number if core_number[n] >= k)
     anti_nodes = (n for n in core_number if core_number[n] < k)
     return (G.subgraph(anti_nodes).copy(), list(nodes))
+	
+	
     
 if __name__ == "__main__":
     FILE_PATH = "./dolphin.txt"
     G = nx.read_edgelist(FILE_PATH)
-    (H, kcore_list) = anti_kcore(G)
-    print H.nodes()
-    print kcore_list
     
-    (M, kcore_list2) = anti_kcore(H)
-    print M.nodes()
-    print kcore_list2
+    '''Set Upper Bound Graph Scale'''
+    node_size ={0.1, 0.2, 0.3, 0.4, 0.5, 0.6}
+    upper_bound = int(0.2 * len(G.nodes()))
+    #print upper_bound
+    
+    resi_node = []
+    G1 = G
+    
+    while len(resi_node) < upper_bound :
+        (H, temp) = anti_kcore(G1)
+        G1 = H
+        resi_node = resi_node + temp
+        
+    print resi_node
+        
+    
+    
 
     
     
