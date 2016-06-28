7e87785feaf3a3d628feb73555fba8930a9fc2f8
Lovingmage <skiphoopwow@gmail.com>
2016-06-28 10:35:20 -0700

    Update SUCD
    Update SUCD

--- Anti Kcore/antikcore.py
+++ Anti Kcore/antikcore.py
@@ -31,67 +31,10 @@
 *
 *  Required Files:
 *  ---------------
-*  Sockets.h, Sockets.cpp, 
-*  Logger.h, Logger.cpp, 
-*  Utilities.h, Utililties.cpp, 
-*  WindowsHelpers.h, WindowsHelpers.cpp
+*	networkx， matplotlib, community, sys, random
 *
 *  Maintenance History:
 *  --------------------
-*  ver 5.1 : 10 Apr 16
-*  - Added sendStream and recvStream to support sending and receiving
-*    file streams.  These simply wrap the native sockets send and recv.
-*  ver 5.0 : 04 Mar 16
-*  - Fixed bugs in Socket test stub, essentially stealing fixes from
-*    ClientTest.cpp and ServerTest.cpp
-*  - Didn't change any code in the Socket library itself
-*  ver 4.9 : 04 Mar 16
-*  - Added a single write statement in Socket::Listener::accept()
-*  ver 4.8 : 22 Feb 16
-*  - Replaced verbose I/O with Logger I/O
-*  - Replaced ApplicationHelpers package with Utilities package
-*  ver 4.7 : 04 Apr 15
-*  - removed testBlockHandling declaration from Socket.cpp ClientHandler.
-*    The implementation had already been removed, I just forgot the declaration.
-*  - added test for INVALID_SOCKET in Socket::recvString.  The omission was
-*    reported by Huanming Fang.  Thanks Huanming.
-*  ver 4.6 : 30 Mar 15
-*  - minor modification to comments, above, and in Socket class implem.
-*  ver 4.5 : 30 Mar 15
-*  - moved SocketListener::start(...) from cpp to h file since it is a
-*    template method.
-*  - renamed ClientProc to ClientHandler
-*  - removed Block operations to avoid binding Socket package to
-*    FileSystem package.  Will add buffer operations to the
-*    FileSystem::File class to match the Socket buffer operations.
-*  - gave ClientHandler a command interpreter to select a test process
-*    - test string tranfers
-*    - test buffer transfers
-*    - client sends a string to select test mode
-*    - test modes are (string, buffer, and stop)
-*  - Created a Verbose class in AppHelpers package that locks stream io.
-*    That helps to keep server and client io text from intermingling.
-*    You can turn verbose mode off which silences output that isn't
-*    marked "always".
-*  - Fixed again the bug which prevented communicating with anything other
-*    than the loopback by adding hints.ai_flags = AI_PASSIVE to
-*    SocketListener member data.
-*  - added more testing
-*  ver 4.4 : 27 Mar 15
-*  - minor changes to comments
-*  - moved ClientHandler into test stub
-*  ver 4.3 : 26 Mar 15
-*  - fixed bug noticed by Tarun Rajput 
-*  - used '0' as terminator.  Should have been '\0'
-*  ver 4.2 : 26 Mar 15
-*  - several small changes to the Socket class interface
-*  ver 4.1 : 25 Mar 15
-*  - fixed connection bug that prevented connecting to anything
-*    other than a loopback (localhost, 127.0.0.1, ::1) by
-*    adding winsock code to SocketConnecter().
-*  - removed low-level code from ClientProc 
-*    (server's client handler callable object)
-*    replaced with code written to Socket interface
 *  Ver 4.0 : 24 Mar 15
 *  - first release of total redesign - had a known bug (see ver 4.1)
 */
